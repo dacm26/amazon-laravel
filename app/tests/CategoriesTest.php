aef803149f49f3d67d8fae7560a9199ed629b2a7
Joel Pineda <joel.pineda@unitec.edu>
2014-10-28 17:04:33 +0000

    CategoriesTest for Model Done

--- app/tests/CategoriesTest.php
+++ app/tests/CategoriesTest.php
@@ -15,4 +15,27 @@ class CategoriesTest extends TestCase {
     $this->assertEquals($category->code,'QA'); 
     $this->assertEquals($category->tax_free,false); 
   }
+  
+  public function testInputEmptyData(){
+    $category = new Category();
+   
+    $this->assertEmpty($category->name); 
+    $this->assertEmpty($category->description); 
+    $this->assertEmpty($category->code); 
+    $this->assertEmpty($category->tax_free); 
+  }
+  
+  public function testInputType(){
+    $category = new Category();
+    
+    $category->name = 'test example name';
+    $category->description = 'Some Description';
+    $category->code = 'QA';
+    $category->tax_free = false;
+    
+    $this->assertInternalType('string',$category->name); 
+    $this->assertInternalType('string',$category->description); 
+    $this->assertInternalType('string',$category->code); 
+    $this->assertInternalType('bool',$category->tax_free);
+  } 
 }
