baee908657341d97d22b7cef576b0584d32dbd55
Joel Pineda <joel.pineda@unitec.edu>
2014-11-10 23:13:35 +0000

    BrandsTest for views done

--- resetDatabase.sql
+++ resetDatabase.sql
@@ -2,3 +2,4 @@ USE amazon;
 DELETE FROM customers;
 DELETE FROM roles WHERE name = 'Gerente Admin';
 DELETE FROM employees WHERE email = 'joelpineda94@gmail.com';
+DELETE FROM brands WHERE name = 'Test Brand New';
