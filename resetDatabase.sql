6acc015e3ca23c7532aad087deb478469cafb2fd
Joel Pineda <joel.pineda@unitec.edu>
2014-11-11 00:02:43 +0000

    CategoriesTest for views done

--- resetDatabase.sql
+++ resetDatabase.sql
@@ -3,3 +3,4 @@ DELETE FROM customers;
 DELETE FROM roles WHERE name = 'Gerente Admin';
 DELETE FROM employees WHERE email = 'joelpineda94@gmail.com';
 DELETE FROM brands WHERE name = 'Test Brand New';
+DELETE FROM categories WHERE code = 'CAT099';
