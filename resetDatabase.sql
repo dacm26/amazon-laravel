a39ba70c7ceea59d097a2f91306c4e841980dfbe
Joel Pineda <joel.pineda@unitec.edu>
2014-11-14 20:22:39 +0000

    ShippersTest for view done

--- resetDatabase.sql
+++ resetDatabase.sql
@@ -5,3 +5,4 @@ DELETE FROM employees WHERE email = 'joelpineda94@gmail.com';
 DELETE FROM brands WHERE name = 'Test Brand New';
 DELETE FROM categories WHERE code = 'CAT099';
 DELETE FROM products WHERE code = 'PTEST01';
+DELETE FROM shippers WHERE email = 'shipperinternational@gmail.com';
