ee7cea849a475dfe6a42dbc598bfbaa7455c8def
Joel Pineda <joel.pineda@unitec.edu>
2014-11-14 21:47:58 +0000

    DiscountsTest for view done

--- resetDatabase.sql
+++ resetDatabase.sql
@@ -6,3 +6,4 @@ DELETE FROM brands WHERE name = 'Test Brand New';
 DELETE FROM categories WHERE code = 'CAT099';
 DELETE FROM products WHERE code = 'PTEST01';
 DELETE FROM shippers WHERE email = 'shipperinternational@gmail.com';
+DELETE FROM discounts WHERE discount = 11 AND datestart = '2014-12-15' AND dateend = '2014-12-21';
