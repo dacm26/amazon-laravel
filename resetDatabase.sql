feb02361030b6795452cd184111f0af8ed7c0076
Joel Pineda <joel.pineda@unitec.edu>
2014-12-01 23:23:38 +0000

    resetDatabase changes

--- resetDatabase.sql
+++ resetDatabase.sql
@@ -1,13 +1,2 @@
 USE amazon;
-DELETE FROM roles WHERE name = 'Gerente Admin';
-DELETE FROM employees WHERE email = 'joelpineda94@gmail.com';
-DELETE FROM brands WHERE name = 'Test Brand New';
-DELETE FROM categories WHERE code = 'CAT099';
-DELETE FROM products WHERE code = 'PTEST01';
-DELETE FROM shippers WHERE email = 'shipperinternational@gmail.com';
-DELETE FROM discounts WHERE discount = 11 AND datestart = '2014-12-15' AND dateend = '2014-12-21';
-DELETE FROM wishlists WHERE customer_id = 3;
-DELETE FROM carts WHERE customer_id = 3;
-DELETE FROM cards WHERE customer_id = 3;
-DELETE FROM orders;
-DELETE FROM order_details;
+DELETE FROM carts WHERE customer_id = 1 AND product_id = 1;
