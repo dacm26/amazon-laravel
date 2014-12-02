53543c3f3ab7e36177c16b263c3ad89e9b0ba1ce
dacm26 <dacm26@gmail.com>
2014-12-02 01:24:55 +0000

    Adding super user

---
+++ app/database/migrations/2014_12_02_005125_add_super_user.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Carbon\Carbon;
+class AddSuperUser extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    $role=Role::create([
+      'name' => 'super'
+    ]);
+    Employee::create([
+      'name' => 'DCJJ User ',
+      'email' => 'dacm8@outlook.com',
+      'birthday' => Carbon::createFromDate(1990, 12, 25),
+      'mobile' => 12345678,
+      'gender' => 'M',
+      'password' => 'admin123',
+      'role_id' => $role->id      
+    ]);
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+     Role::destroy(1);
+     Employee::destroy(1);
+	}
+
+}
